0046aa4bca3a05530dbad0cace01cf2991c3b4aa
Noah S-C <noah@sourcegraph.com>
2023-12-18 21:00:30 +0000

    nix: bump nixpkgs to include http-parser fixes (#31)

--- nix/static-deps.nix
+++ nix/static-deps.nix
@@ -11,21 +11,7 @@ let
   inherit (callPackage ./util.nix { }) mkStatic;
 in
 {
-  http-parser = (mkStatic http-parser).overrideAttrs (oldAttrs: {
-    # needed until upstream PR hits nixpkgs-unstable,
-    # track https://nixpk.gs/pr-tracker.html?pr=254516.
-    # http-parser makefile is a bit incomplete, so fill in the gaps here
-    # to move the static object and header files to the right location
-    # https://github.com/nodejs/http-parser/issues/310.
-    buildFlags = [ "package" ];
-    installTargets = "package";
-    postInstall = ''
-      install -D libhttp_parser.a $out/lib/libhttp_parser.a
-      install -D  http_parser.h $out/include/http_parser.h
-      ls -la $out/lib $out/include
-    '';
-  });
-
+  http-parser = mkStatic http-parser;
   libiconv = mkStatic libiconv;
   openssl = mkStatic openssl;
   openssl_1_1 = mkStatic openssl_1_1;
