d016d6263bc92cfffb1fc5cb0861217092bcf13b
William Bezuidenhout <william.bezuidenhout@sourcegraph.com>
2024-01-09 10:09:11 +0200

    Report workflow actions failures to sentry  (#32)
    * update pr-auditor and report failures for auditor and nix build
    
    * fix checksums
    
    ran All checksums are good!
    
    Part of https://github.com/sourcegraph/sourcegraph/issues/59055

--- nix/helix-core-api.nix
+++ nix/helix-core-api.nix
@@ -4,34 +4,34 @@
     aarch64-darwin = fetchzip {
       name = "helix-core-api";
       url = "https://cdist2.perforce.com/perforce/r23.1/bin.macosx12arm64/p4api-openssl1.1.1.tgz";
-      hash = "sha256-gblxNxyvjGHmrE54178JN4NTvI2EW3a58Yj7KngEr1o=";
+      hash = "sha256-MiRKvW/IbA1AcNpFWCIBGsYomelCLrTU04+Vq5yyUVI=";
     };
     x86_64-darwin = fetchzip {
       name = "helix-core-api";
       url = "https://cdist2.perforce.com/perforce/r23.1/bin.macosx12x86_64/p4api-openssl1.1.1.tgz";
-      hash = "sha256-9H0qbYU9yFcdqooLvXSkqYIiYIX9d+BaSh9dlMo1D5Q=";
+      hash = "sha256-QF69oWI8qqST9ELJrEzx4pFQik/RsEmsRVq7EOo5gt0=";
     };
     x86_64-linux = fetchzip {
       name = "helix-core-api";
       url = "https://cdist2.perforce.com/perforce/r23.1/bin.linux26x86_64/p4api-glibc2.3-openssl1.1.1.tgz";
-      hash = "sha256-SBeHjVuodRlLmAYA7/5qSASmneKaa7acXvhEPTxQd/w=";
+      hash = "sha256-R8oP1uJ70J2XRToLAPAVKxU/VEr2e6LlaR3zn21lKaI=";
     };
   };
   "3.0" = {
     aarch64-darwin = fetchzip {
       name = "helix-core-api";
       url = "https://cdist2.perforce.com/perforce/r23.1/bin.macosx12arm64/p4api-openssl3.tgz";
-      hash = "sha256-s75750zXUxLbx3Bs2kjtGTKI1sKHO+tAQTF2d0dc234=";
+      hash = "sha256-SVAw1ADhd7pwgHprE+J61bp5AiP9ULCw64WeN5MVV9M=";
     };
     x86_64-darwin = fetchzip {
       name = "helix-core-api";
       url = "https://cdist2.perforce.com/perforce/r23.1/bin.macosx12x86_64/p4api-openssl3.tgz";
-      hash = "sha256-uCR91Kbidq8DyJSw+Dpmiyn66LIkveNHZFwCfbBZ6dk=";
+      hash = "sha256-HhSmQWfX9yh734FtG14BzIQY9YliAH9kV5RaQ71T0RU=";
     };
     x86_64-linux = fetchzip {
       name = "helix-core-api";
       url = "https://cdist2.perforce.com/perforce/r23.1/bin.linux26x86_64/p4api-glibc2.3-openssl3.tgz";
-      hash = "sha256-JaFjUjJP1GQ4iFCtuWbhYNonH3PiNlgI0z2L6UsN1JE=";
+      hash = "sha256-oJzHuq8/9PgGi6e1/bxXuaZER5+DR5tmCIN21/fVImQ=";
     };
   };
 }
