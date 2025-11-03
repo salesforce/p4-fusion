e55f2edd60efeafb3cd5ea3cf54e9d3b50ea08af
peter-esik <67009121+peter-esik@users.noreply.github.com>
2025-11-03 17:14:06 +0100

    Handle P4 special characters
    P4V restores the original filenames when syncing a file, but in the command line output of p4 commands the escapes are not restored, therefore the escape sequences "escaped" through p4-fusion into the Git repo filenames. This change resolves this bug.
    
    Co-authored-by: Márton Marczell <72875131+mmarczell-graphisoft@users.noreply.github.com>

---
+++ p4-fusion/utils/p4_helpers.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2022 Salesforce, Inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+#pragma once
+
+#include <string>
+
+std::string P4Unescape(std::string p4path);
